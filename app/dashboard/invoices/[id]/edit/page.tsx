eccfbce7ac8cb88790a7669e182a8c4d819d5690
andreanat <andrea.nmb13@gmail.com>
2026-07-16 09:57:20 -0600

    chapter12

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: {
   params: Promise<{ id: string }>;
@@ -12,6 +13,9 @@ export default async function Page(props: {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+  if (!invoice) {
+  notFound();
+}
 
   return (
     <main>
@@ -26,7 +30,7 @@ export default async function Page(props: {
         ]}
       />
 
-      <Form invoice={invoice!} customers={customers} />
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
